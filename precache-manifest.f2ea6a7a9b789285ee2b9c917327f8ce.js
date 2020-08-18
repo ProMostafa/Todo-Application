aefb00ff339d0a5b6b25cfc8f56fdb938f9ca014
ProMostafa <promostafaeladawy@gmail.com>
2020-08-18 23:17:29 +0200

    Updates

---
+++ precache-manifest.f2ea6a7a9b789285ee2b9c917327f8ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "678586d82cb45508c5259fbaf9a65291",
+    "url": "/Todo-Application/index.html"
+  },
+  {
+    "revision": "7f8e7f7a5e9f5564f3c4",
+    "url": "/Todo-Application/static/css/main.60332991.chunk.css"
+  },
+  {
+    "revision": "c300d24f6045d9e2b430",
+    "url": "/Todo-Application/static/js/2.5a023434.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-Application/static/js/2.5a023434.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f8e7f7a5e9f5564f3c4",
+    "url": "/Todo-Application/static/js/main.16202db2.chunk.js"
+  },
+  {
+    "revision": "a568c60dbfa7edcbfcc6",
+    "url": "/Todo-Application/static/js/runtime-main.4eb65a79.js"
+  }
+]);
